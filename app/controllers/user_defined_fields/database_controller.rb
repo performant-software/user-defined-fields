f149cff90bc63cb948649260b3f0e082d36b5c72
Derek Leadbetter <dleadbetter55@gmail.com>
2022-09-16 13:36:31 -0400

    UDF #9 - Adding /data_types and /tables API endpoints

---
+++ app/controllers/user_defined_fields/database_controller.rb
@@ -0,0 +1,21 @@
+module UserDefinedFields
+  class DatabaseController < ApplicationController
+    def data_types
+      data_types = %w(Boolean Date Number Select String Text)
+      render json: { data_types: data_types }, status: :ok
+    end
+
+    def tables
+      tables = []
+
+      ActiveRecord::Base.connection.tables.map do |model|
+        klass = model.classify
+        next unless klass.safe_constantize&.included_modules&.include?(Fieldable)
+
+        tables << klass.to_s
+      end
+
+      render json: { tables: tables }, status: :ok
+    end
+  end
+end
--- app/models/user_defined_fields/user_defined_field.rb
+++ app/models/user_defined_fields/user_defined_field.rb
@@ -4,6 +4,6 @@ module UserDefinedFields
     belongs_to :defineable, polymorphic: true, optional: true
 
     # Resourceable parameters
-    allow_params :table_name, :column_name, :data_type, :required, :allow_multiple, :options
+    allow_params :table_name, :column_name, :data_type, :required, :allow_multiple, options: []
   end
 end
