fc444441c9abc0f76c49161bdd51dce9ad893d17
Derek Leadbetter <dleadbetter55@gmail.com>
2022-09-20 10:45:01 -0400

    UDF #5 - Adding filter for defineable to user_defined_fields_controller

--- app/controllers/user_defined_fields/user_defined_fields_controller.rb
+++ app/controllers/user_defined_fields/user_defined_fields_controller.rb
@@ -1,5 +1,23 @@
 module UserDefinedFields
   class UserDefinedFieldsController < ApplicationController
     search_attributes :table_name, :column_name
+
+    protected
+
+    def apply_filters(query)
+      query = super
+
+      query = filter_defineable(query)
+
+      query
+    end
+
+    private
+
+    def filter_defineable(query)
+      return query unless params[:defineable_id].present? && params[:defineable_type].present?
+
+      query.where(defineable_id: params[:defineable_id], defineable_type: params[:defineable_type])
+    end
   end
 end
